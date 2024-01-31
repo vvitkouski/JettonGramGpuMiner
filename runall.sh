46441691f1f7a11bfd285aa8dbeece2df95517bc
Artem V <a.vitkouski@gmail.com>
2024-01-31 22:09:44 +0300

    POW miner for ubuntu 18 and script for multi-gpu

---
+++ runall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+i=0
+
+until [ $i -gt 7 ]
+do
+  bgtid=gram$i node send_universal.js --api tonhub --givers 10000 --bin ./pow-miner-cuda --gpu $i >output.$i 2>&1 &
+  echo test$i
+  ((i++))
+  sleep 1;
+done
