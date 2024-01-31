017cb9ad37666c592ecbf63126b7d1bed206dcee
Artem V <a.vitkouski@gmail.com>
2024-01-31 23:37:44 +0300

    Update default givers

--- runall.sh
+++ runall.sh
@@ -5,7 +5,7 @@ i=0
 
 until [ $i -gt 7 ]
 do
-  bgtid=gram$i node send_universal.js --api tonhub --givers 10000 --bin ./pow-miner-cuda --gpu $i >output.$i 2>&1 &
+  bgtid=gram$i node send_universal.js --api tonhub --givers 1000 --bin ./pow-miner-cuda --gpu $i >output.$i 2>&1 &
   echo test$i
   ((i++))
   sleep 1;
