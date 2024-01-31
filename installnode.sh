aa975df28300510c89d78efecb916e979f4c6042
Artem V <a.vitkouski@gmail.com>
2024-01-31 23:02:58 +0300

    Permissions

--- installnode.sh
+++ installnode.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo apt-get -y install nodejs && sudo apt-get install nodejs-dev node-gyp libssl1.0-dev
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
